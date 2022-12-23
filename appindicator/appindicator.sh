569acbc5645dc79b3d62c0d0a1af0df17fa9aea3
Sjoerd de Valk <sjoerddevalk@outlook.com>
2022-12-23 16:32:52 +0100

    added a process to monitor in the default setup

--- appindicator/appindicator.sh
+++ appindicator/appindicator.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ~/Repos/MyGithub/PublicPythonProjects/appindicator
 python3 appindicator.py -i 0 &
-python3 appindicator.py -i 1
+python3 appindicator.py -i 1 &
+python3 appindicator.py -i 2
